61d1ff19760f75399f4dd207cf450523a7f8e857
Han Wang <whxjtu@gmail.com>
2018-03-21 19:49:37 -0500

    Create client.js

---
+++ public/client.js
@@ -0,0 +1,16 @@
+/**
+ * Created by whxjt on 9/17/2017.
+ */
+var search = document.querySelector('[type=search]');
+var code = document.querySelector('pre');
+
+search.addEventListener('keyup', function(){
+    var xhr = new XMLHttpRequest;
+    xhr.open('GET', '/search/' + search.value, true);
+    xhr.onreadystatechange = function(){
+        if (4 == xhr.readyState) {
+            code.textContent = xhr.responseText;
+        }
+    };
+    xhr.send();
+}, false);
